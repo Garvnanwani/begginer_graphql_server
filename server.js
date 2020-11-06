7485a1541c83cd9f7ae66ee138206ccbfe908abc
Garv nanwani <garvnanwani16@gmail.com>
2020-11-06 22:40:16 +0530

    initial server

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 const express = require("express")
+const { graphqlHTTP } = require('express-graphql')
+const ggl = require("graphql-tag")
+const { buildASTSchema } = require("graphql")
+const cors = require("cors")
 
 const app = express()
 
-app.listen(5000, () => console.log("Server is running"))
+const PORT = process.env.PORT || 5000
+
+app.use(cors())
+
+const schema = buildASTSchema(ggl`
+    type Query {
+        hello: String
+    }
+`)
+
+const rootValue = {
+    hello: () => 'Hello, world'
+}
+
+app.use('/graphql', graphqlHTTP({ schema, rootValue }))
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
